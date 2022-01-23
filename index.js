ffec01397a3f3d0b90a257ad284079132d2db10e
Thales Souza <hey@thales.live>
2022-01-22 23:28:31 -0300

    cleaning deadcode

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ app.use(express.json())
 
 const db = require('./models')
 
-// const {Match, Players} = require('./models')
-
 // Route
 
 const router = require('./routes/matchRouter.js')
@@ -23,46 +21,6 @@ app.use((req, res, next) =>{
     console.log('Finish')
     console.timeEnd('Request')
 })
-
-
-// app.get('/matches', (req, res) => {
-//     Match.findAll()
-//         .then((matches) => {
-//         res.send(matches)
-//     })
-//         .catch((err) => {
-//             console.log(err)
-//     })
-// })
-//
-// app.get('/insert', (req, res) => {
-//     Match.create({
-//         map: "de_aztec",
-//         max_players: 16,
-//         ct_players: 8,
-//         t_players: 8,
-//         ct_score: 9,
-//         t_score: 13,
-//         start_time_match: date_ob,
-//         is_match_finished: true
-//     }).catch(err =>{
-//         if (err){
-//             console.log(`Error is: ${err}`)
-//         }
-//     })
-//
-//     res.send('inserted...')
-// })
-//
-// app.get('/players', (req, res) =>{
-//     Players.findAll()
-//         .then((players) =>{
-//             res.send(players)
-//         })
-//         .catch((err) => {
-//             res.send(`Ooops. Error: ${err}`)
-//         })
-// })
 
 db.sequelize.sync().then((req) =>{
     app.listen(3000, () =>{
